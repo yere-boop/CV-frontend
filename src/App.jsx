8dfa0e968ae8c99e9501b1ef0e57fc454670e3d6
adithiachristian7 <adithiachristian@gmail.com>
2024-11-13 15:23:12 +0800

    create vite

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./style.css";
+import Header from "./components/header/index.js";
+import Hero from "./components/hero/hero.js";
+import Skills from "./components/skills/skills.js";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <Hero />
+      <Skills />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/config/firebase/index.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCM7uTyy141592W7Ne581-KX5PfCqQjqW8",
+  authDomain: "portfolio-d7c84.firebaseapp.com",
+  projectId: "portfolio-d7c84",
+  storageBucket: "portfolio-d7c84.firebasestorage.app",
+  messagingSenderId: "332252298963",
+  appId: "1:332252298963:web:945e052fda6f4ee4841e26",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
