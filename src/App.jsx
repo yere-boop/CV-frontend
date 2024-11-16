ef37781dd5f3871193ee5e2aed2a0cb74f1fc6a6
adithiachristian7 <adithiachristian@gmail.com>
2024-11-16 14:58:08 +0800

    menambah loading di app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,51 @@ import About from "./components/about/about.jsx";
 import Skill from "./components/skill/skill.jsx";
 import Project from "./components/project/project.jsx";
 import Footer from "./components/footer/footer.jsx";
+import { useState, useEffect } from "react";
+import { ColorRing } from "react-loader-spinner";
 
-function App() {
+const App = () => {
+  const [loading, setloading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setloading(false);
+    }, 1500);
+  }, []);
+
   return (
-    <div>
-      <Header />
-      <Hero />
-      <About />
-      <Skill />
-      <Project />
-      <Footer />
-    </div>
+    <>
+      {loading && (
+        <div style={styles.blurOverlay}>
+          <ColorRing width="200" color="#007bff" />
+        </div>
+      )}
+      <div>
+        <Header />
+        <Hero />
+        <About />
+        <Skill />
+        <Project />
+        <Footer />
+      </div>
+    </>
   );
-}
+};
+
+const styles = {
+  blurOverlay: {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    backdropFilter: "blur(10px)",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "transparent",
+    zIndex: 9999,
+  },
+};
 
 export default App;
